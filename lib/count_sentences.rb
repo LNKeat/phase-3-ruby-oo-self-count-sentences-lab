873e1f6b6357ca841b4ce13df1a95965bcb23ae8
Laura <lnkeat@gmail.com>
2021-11-18 14:19:53 -0700

    commit tree 01e6b7ed41386d30a913f8eb44b241de35fb7260

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr = self.scan(/([a-zA-Z]\.)|([a-zA-Z]\!)|([a-zA-Z]\?)/)
+    arr.length
   end
 end
